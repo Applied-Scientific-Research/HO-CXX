b3ccf14cc2f790436fa5529f5978325d8477fc25
Mark Stock <markjstock@gmail.com>
2021-05-12 18:22:30 -0400

    Enabled command line argument for input file

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,57 @@
  *
  * (c)2018-21 Applied Scientific Research, Inc.
  *            Mohammad Haji <mhajit@gmail.com>
+ *            Mark J Stock <markjstock@gmail.com>
  *            Based on work by
  *            Adrin Gharakhani <adrin@applied-scientific.com>
  *            Christoper Stone <cpstone@gmail.com>
  */
 
 #include "HO_2D.hpp"
-#include "Mesh.hpp"
 
 #include <iostream>
 
-
-int main()
+int main(int argc, char const *argv[])
 {
-    const std::string input_file_name = "input.dat"; //the file listing all the settings
+    //file listing all the settings, default name
+    std::string input_file_name = "input.dat";
+
+    // grab the mesh file from the command line
+    if (argc == 2) {
+        input_file_name = argv[1];
+    } else if (argc > 2) {
+        std::cout << std::endl << "Usage:" << std::endl;
+        std::cout << "  " << argv[0] << " [input.dat]" << std::endl << std::endl;
+        return -1;
+    }
+
+    // the kitchen sink class
     HO_2D ho_2d;
-    ho_2d.read_input_file(input_file_name); //true means the OCC format. zero means the buildin format of GMSH
-    int success_mesh = ho_2d.setup_mesh(); //if problem type is 10 then read from file, otherwise form a specific predefined mesh based on grid_type
+
+    //true means gmsh file made with opencascade kernel, zero means the built-in format of GMSH
+    ho_2d.read_input_file(input_file_name);
+
+    //if problem type is 10 then read from file, otherwise form a specific predefined mesh based on grid_type
+    int success_mesh = ho_2d.setup_mesh();
     char success_allocate = ho_2d.allocate_arrays();
-    ho_2d.setup_sps_gps(); //set the local coordinates of the solution points, geometry points
+
+    //set the local coordinates of the solution points, geometry points
+    ho_2d.setup_sps_gps();
     ho_2d.read_process_sample_points();
-    ho_2d.form_bases(); //form the sps lagrangian shepe function (&derivatives), gps shape functions (&derivatives), form Radau bases
+
+    //form the sps lagrangian shape function (&derivatives), gps shape functions (&derivatives), form Radau bases
+    ho_2d.form_bases();
     ho_2d.form_metrics();
     ho_2d.setup_IC_BC_SRC();
-    ho_2d.form_Laplace_operator_matrix(); //form the LHS matrix in Laplace discretization in Eigen/AMGCL/HYPRE format. Done only once. (The type of BC comes from BC_switch_psi, so the type of BC for psi can not change over time
-    ho_2d.solve_vorticity_streamfunction(); //solves the vorticity and stream functions equations for all the times
+
+    //form the LHS matrix in Laplace discretization in Eigen/AMGCL/HYPRE format. Done only once.
+    // (The type of BC comes from BC_switch_psi, so the type of BC for psi can not change over time
+    ho_2d.form_Laplace_operator_matrix();
+
+    //solves the vorticity and stream functions equations for all the times
+    ho_2d.solve_vorticity_streamfunction();
+
+    // dealloc and quit
     ho_2d.release_memory();
 }
 
