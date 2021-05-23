eeb5ab71c34a85da7663c9a81fbd6f3cc4a622a4
Mark Stock <markjstock@gmail.com>
2021-05-23 14:11:15 -0400

    Added 4D to memory helper

--- src/MemoryHelper.hpp
+++ src/MemoryHelper.hpp
@@ -10,8 +10,6 @@
 template <class T>
 T** allocate_array(const size_t nx, const size_t ny) {
 
-	//T** array = (T**)malloc(nx * sizeof(T*));
-	//array[0] = (T*)malloc(nx * ny * sizeof(T));
     T** array = new T* [nx];
 	array[0] = new T [nx*ny];
 
@@ -25,8 +23,6 @@ T** allocate_array(const size_t nx, const size_t ny) {
 template <class T>
 void free_array(T** array) {
 	if (array == nullptr) return;
-	//free(array[0]);
-	//free(array);
 	delete[] array[0];
 	delete[] array;
 	return;
@@ -37,9 +33,6 @@ void free_array(T** array) {
 template <class T>
 T*** allocate_array(const size_t nx, const size_t ny, const size_t nz) {
 
-	//T*** array = (T***)malloc(nx * sizeof(T**));
-	//array[0] = (T**)malloc(nx * ny * sizeof(T*));
-	//array[0][0] = (T*)malloc(nx * ny * nz * sizeof(T));
     T*** array = new T** [nx];
 	array[0] = new T* [nx*ny];
 	array[0][0] = new T [nx*ny*nz];
@@ -61,9 +54,50 @@ T*** allocate_array(const size_t nx, const size_t ny, const size_t nz) {
 template <class T>
 void free_array(T*** array) {
 	if (array == nullptr) return;
-	//free(array[0][0]);
-	//free(array[0]);
-	//free(array);
+	delete[] array[0][0];
+	delete[] array[0];
+	delete[] array;
+	return;
+}
+
+// generalized alloc/dealloc for 4D arrays in contiguous memory
+
+template <class T>
+T**** allocate_array(const size_t nx, const size_t ny, const size_t nz, const size_t nq) {
+
+    T**** array = new T*** [nx];
+	array[0] = new T** [nx*ny];
+	array[0][0] = new T* [nx*ny*nz];
+	array[0][0][0] = new T [nx*ny*nz*nq];
+
+	for (size_t i=1; i<nx; ++i) {
+		array[i] = array[0] + i * ny;
+	}
+
+	for (size_t i=0; i<nx; ++i) {
+		if (i != 0) array[i][0] = array[0][0] + i * ny * nz;
+		for (size_t j=1; j<ny; ++j) {
+			array[i][j] = array[i][0] + j * nz;
+		}
+	}
+
+	for (size_t i=0; i<nx; ++i) {
+		if (i != 0) array[i][0][0] = array[0][0][0] + i * ny * nz * nq;
+		for (size_t j=0; j<ny; ++j) {
+			if (j != 0) array[i][j][0] = array[i][0][0] + j * nz * nq;
+			for (size_t k=1; k<nz; ++k) {
+				array[i][j][k] = array[i][j][0] + k * nq;
+			}
+		}
+	}
+
+	return array;
+}
+
+template <class T>
+void free_array(T**** array) {
+	if (array == nullptr) return;
+	delete[] array[0][0][0];
 	delete[] array[0][0];
 	delete[] array[0];
 	delete[] array;
