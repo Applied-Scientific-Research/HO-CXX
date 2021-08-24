e91bc683cb3596d58e9a592a4e9a0e4d9f12b9d9
Mark Stock <markjstock@gmail.com>
2021-08-24 16:50:56 -0400

    Added 5-D array allocator and de-allocator

--- src/MemoryHelper.hpp
+++ src/MemoryHelper.hpp
@@ -104,3 +104,63 @@ void free_array(T**** array) {
 	return;
 }
 
+// generalized alloc/dealloc for 5D -- yes, 5D -- arrays in contiguous memory
+
+template <class T>
+T***** allocate_array(const size_t nx, const size_t ny, const size_t nz,
+						const size_t nq, const size_t nr) {
+
+    T***** array = new T**** [nx];
+	array[0] = new T*** [nx*ny];
+	array[0][0] = new T** [nx*ny*nz];
+	array[0][0][0] = new T* [nx*ny*nz*nq];
+	array[0][0][0][0] = new T [nx*ny*nz*nq*nr];
+
+	for (size_t i=1; i<nx; ++i) {
+		array[i] = array[0] + i * ny;
+	}
+
+	for (size_t i=0; i<nx; ++i) {
+		if (i != 0) array[i][0] = array[0][0] + i * ny * nz;
+		for (size_t j=1; j<ny; ++j) {
+			array[i][j] = array[i][0] + j * nz;
+		}
+	}
+
+	for (size_t i=0; i<nx; ++i) {
+		if (i != 0) array[i][0][0] = array[0][0][0] + i * ny * nz * nq;
+		for (size_t j=0; j<ny; ++j) {
+			if (j != 0) array[i][j][0] = array[i][0][0] + j * nz * nq;
+			for (size_t k=1; k<nz; ++k) {
+				array[i][j][k] = array[i][j][0] + k * nq;
+			}
+		}
+	}
+
+	for (size_t i=0; i<nx; ++i) {
+		if (i != 0) array[i][0][0][0] = array[0][0][0][0] + i * ny * nz * nq * nr;
+		for (size_t j=0; j<ny; ++j) {
+			if (j != 0) array[i][j][0][0] = array[i][0][0][0] + j * nz * nq * nr;
+			for (size_t k=0; k<nz; ++k) {
+				if (k != 0) array[i][j][k][0] = array[i][j][0][0] + k * nq * nr;
+				for (size_t l=1; l<nq; ++l) {
+					array[i][j][k][l] = array[i][j][k][0] + l * nr;
+				}
+			}
+		}
+	}
+
+	return array;
+}
+
+template <class T>
+void free_array(T***** array) {
+	if (array == nullptr) return;
+	delete[] array[0][0][0][0];
+	delete[] array[0][0][0];
+	delete[] array[0][0];
+	delete[] array[0];
+	delete[] array;
+	return;
+}
+
