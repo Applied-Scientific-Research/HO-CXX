6ecc0b1319ad735807a12bc90862c9a2f41b1da6
Mark Stock <markjstock@gmail.com>
2021-10-04 13:31:18 -0400

    Tab-ified main, sync'd with HO-3D

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
  * main.cpp - Driver code for HO-CXX, a high-order solver in vorticity variables
  *
- * (c)2018-20 Applied Scientific Research, Inc.
+ * (c)2018-21 Applied Scientific Research, Inc.
  *            Mohammad Haji <mhajit@gmail.com>
  *            Based on work by
  *            Adrin Gharakhani <adrin@applied-scientific.com>
@@ -15,48 +15,54 @@
 
 int main(int argc, char const *argv[])
 {
-    //file listing all the settings, default name
-    std::string input_file_name = "input.dat";
+	// file listing all the settings, default name
+	std::string input_file_name = "input.dat";
 
-    // grab the mesh file from the command line
-    if (argc == 2) {
-        input_file_name = argv[1];
-    } else if (argc > 2) {
-        std::cout << std::endl << "Usage:" << std::endl;
-        std::cout << "  " << argv[0] << " [input.dat]" << std::endl << std::endl;
-        return -1;
-    }
+	// grab the input file from the command line
+	if (argc == 2) {
+		input_file_name = argv[1];
+	} else if (argc > 2) {
+		std::cout << std::endl << "Usage:" << std::endl;
+		std::cout << "  " << argv[0] << " [input.dat]" << std::endl << std::endl;
+		return -1;
+	}
 
-    // the kitchen sink class
-    HO_2D ho_2d;
+	// the kitchen sink class
+	HO_2D ho_solver;
 
-    //true means gmsh file made with opencascade kernel, zero means the built-in format of GMSH
-    ho_2d.read_input_file(input_file_name);
+	// return true means gmsh file made with opencascade kernel, zero means the built-in format of GMSH
+	ho_solver.read_input_file(input_file_name);
 
-    auto error_mesh = ho_2d.setup_mesh();
+	auto error_mesh = ho_solver.setup_mesh();
 	if (error_mesh) {
 		std::cout << std::endl << "Mesh file (" << input_file_name << ") not processed correctly!" << std::endl;
-        return error_mesh;
-    }
-    auto error_allocate = ho_2d.allocate_arrays();
+		return error_mesh;
+	}
+
+	auto error_allocate = ho_solver.allocate_arrays();
 	if (error_allocate) {
 		std::cout << std::endl << "Arrays not allocated correctly!" << std::endl;
-        return error_allocate;
-    }
+		return error_allocate;
+	}
 
-    //set the local coordinates of the solution points, geometry points
-    ho_2d.setup_sps_gps();
-    ho_2d.read_process_sample_points();
+	// set the local coordinates of the solution points, geometry points
+	ho_solver.setup_sps_gps();
 
-    //form the sps lagrangian shape function (&derivatives), gps shape functions (&derivatives), form Radau bases
-    ho_2d.form_bases();
-    ho_2d.form_metrics();
-    ho_2d.setup_IC_BC_SRC();
+	// read and process the sampling points
+	ho_solver.read_process_sample_points();
 
-    //form the LHS matrix in Laplace discretization in Eigen/AMGCL/HYPRE format. Done only once.
-    // (The type of BC comes from BC_switch_psi, so the type of BC for psi can not change over time
-    ho_2d.form_Laplace_operator_matrix();
+	// form the sps lagrangian shape function (&derivatives), gps shape functions (&derivatives), form Radau bases
+	ho_solver.form_bases();
+	ho_solver.form_metrics();
+	ho_solver.setup_IC_BC_SRC();
 
-    //solves the vorticity and stream functions equations for all the times
-    ho_2d.solve_vorticity_streamfunction();
+	// form the LHS matrix in Laplace discretization in Eigen/AMGCL/HYPRE format. Done only once.
+	// (The type of BC comes from BC_switch_psi, so the type of BC for psi can not change over time
+	ho_solver.form_Laplace_operator_matrix();
+
+	// solves the vorticity and stream functions equations for all the times
+	ho_solver.solve_vorticity_streamfunction();
+
+	// return success
+	return 0;
 }
